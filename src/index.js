0b86a5902bf31d7412eb326ae158d48bde5746ee
Christine Lee <chriistinelee@gmail.com>
2020-09-28 00:43:49 -0700

    relocate App to containers folder

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import { App } from 'components';
+import { App } from 'containers';
 import * as serviceWorker from 'serviceWorker';
 import 'styles.scss';
 import './index.scss';

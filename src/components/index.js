8fd6702c0902031f6a85a88b3ef9581230c85425
Christine Lee <chriistinelee@gmail.com>
2020-10-01 22:24:59 -0700

    create a card component that displays a product in the cart and relocate all cards to Card folder

--- src/components/index.js
+++ src/components/index.js
@@ -2,4 +2,5 @@ export { default as Navbar } from './Navbar/Navbar';
 export { default as Cart } from './Cart/Cart';
 export { default as Shop } from './Shop/Shop';
 export { default as Products } from './Products/Products';
-export { default as ShopProductCard } from './ProductCard/ShopProductCard';
+export { default as ShopProductCard } from './Card/ShopProductCard';
+export { default as CartItemCard } from './Card/CartItemCard';

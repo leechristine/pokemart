d55dd18e43f278b96f1d7c3345a14290788ec388
Christine Lee <chriistinelee@gmail.com>
2020-10-01 20:16:02 -0700

    create a card component that displays a product in the shop

---
+++ src/components/ProductCard/ShopProductCard.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+import './ShopProductCard.scss';
+
+function ShopProductCard({ productData }) {
+  return (
+    <div className="product-card">
+      <div className="product-image">
+        <img src={ productData.image } alt={ productData.title } />
+      </div>
+      <div className="product-info">
+        <div className="product-title">{ productData.title }</div>
+        <div className="product-desc">{ productData.description }</div>
+        <div className="product-price">¥{ productData.price }</div>
+      </div>
+      <div className="product-actions">
+        <button>add to cart</button>
+      </div>
+    </div>
+  );
+}
+
+export default ShopProductCard;

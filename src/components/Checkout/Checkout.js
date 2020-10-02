759d120463e55609c6d411a036dce89ae18022c9
Christine Lee <chriistinelee@gmail.com>
2020-10-02 02:31:27 -0700

    connect redux to Checkout to display cart summary (cart total, cart subtotal)

--- src/components/Checkout/Checkout.js
+++ src/components/Checkout/Checkout.js
@@ -1,20 +1,36 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
+import { connect } from 'react-redux';
 import './Checkout.scss';
 
-function Checkout() {
+function Checkout({ cart }) {
+  const [cartSubtotal, setCartSubtotal] = useState(0);
+  const [cartTotal, setCartTotal] = useState(0);
+
+  useEffect(() => {
+    let subtotal = 0;
+    cart.forEach((item) => {
+      subtotal += item.quantity * item.price;
+    });
+
+    setCartSubtotal(subtotal);
+
+    let total = subtotal;
+    setCartTotal(total);
+  }, [cart, cartTotal, cartSubtotal]);
+
   return (
     <div className="checkout-container">
-      <div className="checkout-title">Order Summary</div>
+      <div className="checkout-title">Cart Summary</div>
       <div className="checkout-summary">
         <div className="checkout-subtotal">
           <div className="label">Subotal</div>
-          <div className="amount"></div>
+          <div className="amount">¥{ cartSubtotal }</div>
         </div>
       </div>
       <div className="checkout-total">
         <div className="label">Total</div>
-        <div className="amount"></div>
+        <div className="amount">¥{ cartTotal }</div>
       </div>
       <div className="checkout-actions">
         <button>checkout</button>
@@ -23,4 +39,10 @@ function Checkout() {
   );
 }
 
-export default Checkout;
+const mapStateToProps = (state) => {
+  return {
+    cart: state.shop.cart
+  };
+};
+
+export default connect(mapStateToProps)(Checkout);

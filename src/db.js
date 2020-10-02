60b6a09bff0c0d58de6042bb3dd7795552b83fdc
Christine Lee <chriistinelee@gmail.com>
2020-10-01 18:43:31 -0700

    create data file and add product data

---
+++ src/db.js
@@ -0,0 +1,42 @@
+const db = {
+  products: [
+    {
+      id: 1,
+      title: "Poké Ball",
+      description:
+        "A device for catching wild Pokémon. It is thrown like a ball at the target. It is designed as a capsule system.",
+      price: 200,
+      image:
+        "https://img.pokemondb.net/sprites/items/poke-ball.png",
+    },
+    {
+      id: 2,
+      title: "Hyper Potion",
+      description:
+        "Restores 120 HP.",
+      price: 1500,
+      image:
+        "https://img.pokemondb.net/sprites/items/hyper-potion.png",
+    },
+    {
+      id: 3,
+      title: "Antidote",
+      description:
+        "A spray-type medicine. It lifts the effect of poison from one Pokémon.",
+      price: 200,
+      image:
+        "https://img.pokemondb.net/sprites/items/antidote.png",
+    },
+    {
+      id: 4,
+      title: "Escape Rope",
+      description:
+        "A long, durable rope. Use it to escape instantly from a cave or a dungeon.",
+      price: 300,
+      image:
+        "https://img.pokemondb.net/sprites/items/escape-rope.png",
+    },
+  ]
+};
+
+export default db;

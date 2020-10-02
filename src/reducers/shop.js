1555b33524d753e4c104cf2cb4dfef2cd32cf39b
Christine Lee <chriistinelee@gmail.com>
2020-10-01 21:10:41 -0700

    implement reducer logic for shop

--- src/reducers/shop.js
+++ src/reducers/shop.js
@@ -9,15 +9,51 @@ const INITIAL_STATE = {
 const shopReducer = (state = INITIAL_STATE, action) => {
   switch(action.type) {
     case shopVars.ADD_TO_CART:
-      return {};
+      // get item's data from products
+      const item = state.products.find(product => product.id === action.id);
+      // check if item is already in cart
+      const inCart = state.cart.find(item =>
+        item.id === action.id ? true : false
+      );
+      // if item is in cart, update quantity; else, add item to cart
+      return {
+        ...state,
+        cart: inCart
+        ? state.cart.map((item) =>
+          item.id === action.id
+            ? { ...item, quantity: item.quantity + 1 }
+            : item
+          )
+        : [...state.cart, { ...item, quantity: 1 }],
+      };
     case shopVars.REMOVE_FROM_CART:
-      return {};
+      return {
+        ...state,
+        cart: state.cart.filter((item) => item.id !== action.id),
+      };
     case shopVars.ADD_QUANTITY:
-      return {};
+      return {
+        ...state,
+        cart: state.cart.map((item) =>
+          item.id === action.id
+            ? { ...item, quantity: item.quantity + 1 }
+            : item
+        ),
+      };
     case shopVars.SUB_QUANTITY:
-      return {};
+      return {
+        ...state,
+        cart: state.cart.map((item) =>
+          item.id === action.id && item.id > 0
+            ? { ...item, quantity: item.quantity - 1 }
+            : item
+        ),
+      };
     case shopVars.EMPTY_CART:
-      return {};
+      return {
+        ...state,
+        cart: [],
+      };
     default:
       return state;
   }

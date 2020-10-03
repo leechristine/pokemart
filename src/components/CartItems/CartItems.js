70faa9a195a0d902c6af8cd8cd6965deb3670b21
Christine Lee <chriistinelee@gmail.com>
2020-10-03 05:10:08 -0700

    integrate emptying existing cart

--- src/components/CartItems/CartItems.js
+++ src/components/CartItems/CartItems.js
@@ -5,28 +5,30 @@ import { connect } from 'react-redux';
 import { faCartArrowDown } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
+import { emptyCart } from 'actions/shop';
 import { CartItemCard } from 'components';
 import './CartItems.scss';
 
-function CartItems({ cart }) {
+function CartItems({ cart, emptyCart }) {
   const isCartEmpty = (cart.length === 0);
 
   return (
     <div className="cart-items-container">
-      {isCartEmpty &&
-        <div className="empty-cart-action">
-          <div className="action-dialogue">Your cart is empty!</div>
-          <NavLink className="action-link" to='/'>
-            <FontAwesomeIcon className="action-link-icon" icon={ faCartArrowDown } />
-            <div className="action-link-text">Start Shopping</div>
-          </NavLink>
-        </div>
-      }
       {cart.map(cartItem => (
         <div className="product-container">
           <CartItemCard key={cartItem.id} productData={ cartItem } />
         </div>
       ))}
+      {isCartEmpty
+        ? <div className="to-shop-action">
+            <div className="action-dialogue">Your cart is empty!</div>
+            <NavLink className="action-link" to='/'>
+              <FontAwesomeIcon className="action-link-icon" icon={ faCartArrowDown } />
+              <div className="action-link-text">Start Shopping</div>
+            </NavLink>
+          </div>
+        : <div className="empty-cart-action" onClick={() => emptyCart()}>empty cart</div>
+      }
     </div>
   );
 }
@@ -37,4 +39,10 @@ const mapStateToProps = (state) => {
   };
 };
 
-export default connect(mapStateToProps)(CartItems);
+const mapDispatchToProps = (dispatch) => {
+  return {
+    emptyCart: () => dispatch(emptyCart())
+  };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(CartItems);

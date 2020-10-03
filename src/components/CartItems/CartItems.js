1f5f6d707af729ff399484ff5c5d9b4eea48a7de
Christine Lee <chriistinelee@gmail.com>
2020-10-03 04:49:46 -0700

    style CartItems component

--- src/components/CartItems/CartItems.js
+++ src/components/CartItems/CartItems.js
@@ -1,12 +1,27 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 import { connect } from 'react-redux';
+
+import { faCartArrowDown } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 import { CartItemCard } from 'components';
 import './CartItems.scss';
 
 function CartItems({ cart }) {
+  const isCartEmpty = (cart.length === 0);
+
   return (
     <div className="cart-items-container">
+      {isCartEmpty &&
+        <div className="empty-cart-action">
+          <div className="action-dialogue">Your cart is empty!</div>
+          <NavLink className="action-link" to='/'>
+            <FontAwesomeIcon className="action-link-icon" icon={ faCartArrowDown } />
+            <div className="action-link-text">Start Shopping</div>
+          </NavLink>
+        </div>
+      }
       {cart.map(cartItem => (
         <div className="product-container">
           <CartItemCard key={cartItem.id} productData={ cartItem } />

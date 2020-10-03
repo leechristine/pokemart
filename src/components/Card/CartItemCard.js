21081b18123958825ff74930c6c61a9a92c5517d
Christine Lee <chriistinelee@gmail.com>
2020-10-03 04:52:01 -0700

    style CartItemCard component

--- src/components/Card/CartItemCard.js
+++ src/components/Card/CartItemCard.js
@@ -1,6 +1,9 @@
 import React, { useState, useEffect } from 'react';
 import { connect } from 'react-redux';
 
+import { faTrashAlt, faPlus, faMinus } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
 import { removeFromCart, addQuantity, subQuantity } from 'actions/shop';
 import './CartItemCard.scss';
 
@@ -12,22 +15,30 @@ function CartItemCard({ productData, removeFromCart, addQuantity, subQuantity })
   }, [productData, productQuantity]);
 
   return (
-    <div className="product-card">
-      <div className="product-image">
-        <img src={ productData.image } alt={ productData.title } />
-      </div>
-      <div className="product-info">
-        <div className="product-title">{ productData.title }</div>
-        <div className="product-desc">{ productData.description }</div>
-        <div className="product-price">¥{ productData.price }</div>
+    <div className="cart-product-card">
+      <div className="product-main">
+        <div className="product-image">
+          <img src={ productData.image } alt={ productData.title } />
+        </div>
+        <div className="product-info">
+          <div className="product-title">{ productData.title }</div>
+          <div className="product-desc">{ productData.description }</div>
+          <div className="product-price">¥{ productData.price }</div>
+        </div>
       </div>
       <div className="product-actions">
         <div className="product-quantity-container">
-          <button onClick={() => subQuantity(productData.id)}>-</button>
+          <div className="sub-quantity" onClick={() => subQuantity(productData.id)}>
+            <FontAwesomeIcon className="action-icon" icon={ faMinus } />
+          </div>
           <div className="product-quantity">{ productQuantity }</div>
-          <button onClick={() => addQuantity(productData.id)}>+</button>
+          <div className="add-quantity" onClick={() => addQuantity(productData.id)}>
+            <FontAwesomeIcon className="action-icon" icon={ faPlus } />
+          </div>
         </div>
-        <button onClick={() => removeFromCart(productData.id)}>remove from cart</button>
+        <div className="remove-from-cart" onClick={() => removeFromCart(productData.id)}>
+          <FontAwesomeIcon className="action-icon" icon={ faTrashAlt } />
+        </div>
       </div>
     </div>
   );

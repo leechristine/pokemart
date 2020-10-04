e7d31ece85e98f29b4afe38a73f0fbc1d10e582b
Christine Lee <chriistinelee@gmail.com>
2020-10-03 22:31:30 -0700

    add product count to shop

--- src/components/Products/Products.js
+++ src/components/Products/Products.js
@@ -1,17 +1,37 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { connect } from 'react-redux';
 
 import { ShopProductCard } from 'components';
 import './Products.scss';
 
 function Products({ products }) {
+  const [productCount, setProductCount] = useState(0);
+
+  useEffect(() => {
+    let count = 0;
+    products.forEach((item) => {
+      count += 1;
+    });
+    
+    setProductCount(count);
+  }, [products, productCount]);
+
   return (
     <div className="products-container">
-      {products.map(product => (
-        <div className="product-container">
-          <ShopProductCard key={product.id} productData={ product } />
+      <div className="products-navbar">
+        <div className="products-category">All Products</div>
+        <div className="products-total">
+          <div className="products-total-amount">{ productCount }</div>
+          <div className="products-total-label">&nbsp;items</div>
         </div>
-      ))}
+      </div>
+      <div className="products">
+        {products.map(product => (
+          <div className="product-container">
+            <ShopProductCard key={product.id} productData={ product } />
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
